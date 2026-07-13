d41fa46478cb3bd968a3b5305bf7578628ec3caf
yasaswinik12 <61104365+yasaswinik12@users.noreply.github.com>
2026-07-13 02:21:31 -0500

    Add: sql-common-table-expressions - submission-5

---
+++ SQL/sql-common-table-expressions/submission-5.sql
@@ -0,0 +1,47 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+with filtered_customers as (
+    select customer_id, MAX(price) as max_price from orders group by customer_id having MAX(price) < 100)
+select name from customers 
+join filtered_customers on customers.id=filtered_customers.customer_id 
+order by name
+
+
+
+
+
+
+
+
+
+
+
